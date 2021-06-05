619eca155cf349424753a7015baab0fc20bc35ad
maskalev <ya.maskalev@yandex.ru>
2021-06-06 01:50:44 +0500

    Add Recipe model
    Add Recipe model.
    Add link between recipes and ingredients.
    Add autocomplete to slugs.

--- apps/recipes/admin.py
+++ apps/recipes/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.recipes.models import Ingredient, Recipe, Tag, RecipeIngredients
+
+
+class RecipeIngredientsInline(admin.TabularInline):
+    """
+    Inline class for RecipeIngredients model.
+    """
+    model = Recipe.ingredients.through
+    extra = 1
+
+
+class RecipeAdmin(admin.ModelAdmin):
+    """
+    Admin class for Recipe model.
+    """
+    prepopulated_fields = {'slug': ('title',)}
+    inlines = [
+        RecipeIngredientsInline
+    ]
+
+
+class TagAdmin(admin.ModelAdmin):
+    """
+    Admin class for Tag model.
+    """
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Ingredient)
+admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(RecipeIngredients)
--- apps/recipes/models.py
+++ apps/recipes/models.py
@@ -1,4 +1,7 @@
+from django.core.validators import MinValueValidator
 from django.db import models
+
+from apps.users.models import User
 
 
 class Ingredient(models.Model):
@@ -28,8 +31,14 @@ class Tag(models.Model):
     Tag model (breakfast, lunch and dinner).
     """
     title = models.CharField(
-        max_length=255,
+        max_length=20,
         verbose_name='Tag title'
+    )
+    slug = models.SlugField(
+        max_length=20,
+        db_index=True,
+        unique=True,
+        verbose_name='Tag slug',
     )
 
     def __str__(self):
@@ -38,3 +47,90 @@ class Tag(models.Model):
     class Meta:
         verbose_name = 'Tag'
         verbose_name_plural = 'Tags'
+
+
+class Recipe(models.Model):
+    """
+    Recipe model.
+    """
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Title',
+    )
+    description = models.TextField(
+        verbose_name='Description',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipes',
+        verbose_name='Author',
+    )
+    tags = models.ManyToManyField(
+        'Tag',
+        related_name='recipes',
+        verbose_name='Тэги',
+    )
+    image = models.ImageField(
+        upload_to='recipes/',
+        verbose_name='Image',
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Cooking time, min',
+    )
+    ingredients = models.ManyToManyField(
+        'Ingredient',
+        through='RecipeIngredients',
+        verbose_name='Ingredients',
+    )
+    pub_date = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name='Publication date',
+    )
+    slug = models.SlugField(
+        max_length=250,
+        db_index=True,
+        unique=True,
+        verbose_name='Recipe slug',
+    )
+
+    def __str__(self):
+        return f'{self.title}, {self.author}, {self.pub_date}'
+
+    class Meta:
+        ordering = ('-pub_date',)
+        verbose_name = 'Recipe'
+        verbose_name_plural = 'Recipes'
+
+
+class RecipeIngredients(models.Model):
+    """
+    Link between recipes and ingredients.
+    """
+    recipe = models.ForeignKey(
+        'Recipe',
+        on_delete=models.CASCADE,
+        verbose_name='Recipe',
+    )
+    ingredient = models.ForeignKey(
+        'Ingredient',
+        on_delete=models.CASCADE,
+        verbose_name='Ingredient',
+    )
+    quantity = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=(MinValueValidator(0),),
+        verbose_name='Quantity',
+    )
+
+    def __str__(self):
+        return f'{self.recipe}, {self.ingredient}, {self.quantity}'
+
+    class Meta:
+        verbose_name_plural = 'RecipesIngredients'
+        verbose_name = 'RecipeIngredients'
+        constraints = [
+            models.UniqueConstraint(fields=('recipe', 'ingredient'),
+                                    name='unique_link'),
+        ]
