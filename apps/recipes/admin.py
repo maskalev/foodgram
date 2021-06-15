c848ded99c41b1b60aa14f993df6b176087cbf13
maskalev <ya.maskalev@yandex.ru>
2021-06-15 12:48:31 +0500

    Refactor recipes admin
    Add @admin.register.
    Add search anf filter fields.

--- apps/recipes/admin.py
+++ apps/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from apps.recipes.models import (Ingredient, Recipe, Tag, RecipeIngredients,
-                                 Favorite, Purchase, Follow)
+from apps.recipes.models import (Ingredient, Tag, RecipeIngredients,
+                                 Favorite, Purchase, Follow, Recipe)
 
 
 class RecipeIngredientsInline(admin.TabularInline):
@@ -12,26 +12,61 @@ class RecipeIngredientsInline(admin.TabularInline):
     extra = 1
 
 
+@admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
     """
     Admin class for Recipe model.
     """
+    list_display = (
+        'author',
+        'title',
+        'slug',
+    )
+    search_fields = (
+        'title',
+    )
+    list_filter = (
+        'author',
+        'title',
+        'tags',
+    )
     prepopulated_fields = {'slug': ('title', 'author')}
     inlines = [
-        RecipeIngredientsInline
+        RecipeIngredientsInline,
     ]
 
 
+@admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     """
     Admin class for Tag model.
     """
+    list_display = (
+        'title',
+        'slug',
+        'color',
+    )
+    search_fields = (
+        'title',
+        'slug',
+    )
     prepopulated_fields = {'slug': ('title',)}
 
 
-admin.site.register(Ingredient)
-admin.site.register(Recipe, RecipeAdmin)
-admin.site.register(Tag, TagAdmin)
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+        'unit',
+    )
+    search_fields = (
+        'title',
+    )
+    list_filter = (
+        'title',
+    )
+
+
 admin.site.register(RecipeIngredients)
 admin.site.register(Favorite)
 admin.site.register(Purchase)
