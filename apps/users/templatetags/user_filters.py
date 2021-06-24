a0eb5436ae1c79372069158246a616b30cf93ddc
maskalev <ya.maskalev@yandex.ru>
2021-06-24 22:32:29 +0500

    Delete active_page filter

--- apps/users/templatetags/user_filters.py
+++ apps/users/templatetags/user_filters.py
@@ -7,20 +7,6 @@ from apps.recipes.models import Favorite, Follow, Purchase
 register = template.Library()
 
 
-@register.simple_tag
-def active_page(request, view_name):
-    """
-    Mark active page.
-    """
-    if not request:
-        return ''
-    try:
-        return 'nav__item_active' if resolve(
-            request.path_info).url_name == view_name else ''
-    except Resolver404:
-        return ''
-
-
 @register.simple_tag(takes_context=True)
 def url_replace(context, **kwargs):
     """
