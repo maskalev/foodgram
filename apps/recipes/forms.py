2cbea6b857f5bfe596ed09c03b704e5840246a84
maskalev <ya.maskalev@yandex.ru>
2021-07-10 16:34:56 +0500

    Delete exception from get_ingredients

--- apps/recipes/forms.py
+++ apps/recipes/forms.py
@@ -80,10 +80,11 @@ class RecipeForm(ModelForm):
         for key, ingredient_name in self.data.items():
             if key.startswith('nameIngredient'):
                 ingredient_value = self.data['valueIngredient' + key[14:]]
-                try:
-                    self.ingredients[ingredient_name] = float(ingredient_value)
-                except ValueError:
-                    self.ingredients[ingredient_name] = None
+                self.ingredients[ingredient_name] = float(ingredient_value)
+                # try:
+                #     self.ingredients[ingredient_name] = float(ingredient_value)
+                # except ValueError:
+                #     self.ingredients[ingredient_name] = None
 
     class Meta:
         model = Recipe
