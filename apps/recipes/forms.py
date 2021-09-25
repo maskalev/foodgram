4975357772655f6521becc27f1ef2af387d28fed
maskalev <ya.maskalev@yandex.ru>
2021-09-25 08:00:57 +0500

    Delete ValidationError from clean method

--- apps/recipes/forms.py
+++ apps/recipes/forms.py
@@ -1,6 +1,6 @@
 from uuid import uuid4
 
-from django.core.exceptions import BadRequest, ValidationError
+from django.core.exceptions import BadRequest
 from django.db import IntegrityError, transaction
 from django.forms import (CheckboxSelectMultiple, ModelForm, SelectMultiple,
                           Textarea)
