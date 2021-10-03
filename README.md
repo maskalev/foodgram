277d2c943df6ab4c16b68af7ffc31cad2404ec17
maskalev <ya.maskalev@yandex.ru>
2021-10-03 21:36:10 +0500

    Add API documentation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ![YAMdb workflow](https://github.com/maskalev/foodgram-project-react/actions/workflows/main.yml/badge.svg)
 # foodgram-project
 http://84.201.154.101/
+
+Документация API доступна на http://84.201.154.101/swagger/ или http://84.201.154.101/redoc/
--- foodgram/urls.py
+++ foodgram/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import handler400, handler404, handler500
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 
 from foodgram import settings
 
@@ -9,6 +12,18 @@ handler400 = 'apps.recipes.views.bad_request' # noqa
 handler404 = 'apps.recipes.views.page_not_found' # noqa
 handler500 = 'apps.recipes.views.server_error' # noqa
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,6 +31,10 @@ urlpatterns = [
     path('auth/', include('django.contrib.auth.urls')),
     path('api/', include('apps.api.urls')),
     path('about/', include('apps.about.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0),
+         name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0),
+         name='schema-redoc'),
     path('', include('apps.recipes.urls')),
 ]
 
